9a9edbb00610882dd35c86a11894a176c1908645
FernandoRivas26 <32744463+FernandoRivas26@users.noreply.github.com>
2021-11-28 19:50:13 -0600

    Doc.

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ let server = http.createServer({
 
 }, app)
 
+//redirect this routes into the front end build.
 app.get(["/", "/registro", "/donadores", "/reportes"], (req, res) => {
     res.sendFile(path.join(__dirname, 'server', 'build', 'index.html'))
 });
-
 
 server.listen(3000, function () {
     const d = new Date();
